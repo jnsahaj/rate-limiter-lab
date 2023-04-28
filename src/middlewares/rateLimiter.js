a533d882f461f550625cd6a354afaa7a5a77a4fa
Sahaj-jj <sahajjain13@gmail.com>
2023-04-28 17:55:12 +0530

    refactor: Refactor rateLimiterMiddleware

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -3,45 +3,46 @@ import redisClient from "../redis.js";
 
 const DEFAULT_SCHEME = "sliding_window";
 
-const rateLimiter = async (req, res, next) => {
-    const rateLimiter = new RateLimiter({
-        redisClient,
-    });
+const rateLimiterMiddleware = async (req, res, next) => {
+    try {
+        const rateLimiter = new RateLimiter({ redisClient });
+        const limiterParams = await redisClient.hgetall("limiterParams");
 
-    const limiterParams = await redisClient.hgetall("limiterParams");
+        const scheme = limiterParams.scheme || DEFAULT_SCHEME;
 
-    const scheme = limiterParams.scheme || DEFAULT_SCHEME;
+        let isAllowed = false;
 
-    if (scheme === "sliding_window") {
-        const { max_requests_per_window, window_size_in_ms } = limiterParams;
-        const isAllowed = await rateLimiter.slidingWindow({
-            key: req.ip,
-            maxRequestsPerWindow:
-                parseInt(max_requests_per_window) || undefined,
-            windowSizeInMs: parseInt(window_size_in_ms) || undefined,
-        });
+        if (scheme === "sliding_window") {
+            const { max_requests_per_window, window_size_in_ms } =
+                limiterParams;
+
+            isAllowed = await rateLimiter.slidingWindow({
+                key: req.ip,
+                maxRequestsPerWindow:
+                    parseInt(max_requests_per_window) || undefined,
+                windowSizeInMs: parseInt(window_size_in_ms) || undefined,
+            });
+        } else if (scheme === "token_bucket") {
+            const { bucket_size, refill_rate_in_ms } = limiterParams;
+
+            isAllowed = await rateLimiter.tokenBucket({
+                key: req.ip,
+                bucketSize: parseInt(bucket_size) || undefined,
+                refillRateInMs: parseInt(refill_rate_in_ms) || undefined,
+            });
+        } else {
+            throw new Error(`Unknown rate limiter scheme: ${scheme}`);
+        }
 
         if (isAllowed) {
             return next();
         } else {
             return res.status(429).send("Too Many Requests");
         }
-    }
-
-    if (scheme === "token_bucket") {
-        const { bucket_size, refill_rate_in_ms } = limiterParams;
-        const isAllowed = await rateLimiter.tokenBucket({
-            key: req.ip,
-            bucketSize: parseInt(bucket_size) || undefined,
-            refillRateInMs: parseInt(refill_rate_in_ms) || undefined,
-        });
-
-        if (isAllowed) {
-            return next();
-        } else {
-            return res.status(429).send("Too Many Requests");
-        }
+    } catch (error) {
+        console.error(`Error in rate limiter middleware: ${error}`);
+        return res.status(500).send("Internal Server Error");
     }
 };
 
-export default rateLimiter;
+export default rateLimiterMiddleware;
