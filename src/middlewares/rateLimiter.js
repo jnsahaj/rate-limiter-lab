ba028d7475aa4c06055c926a280737f40bc36576
Sahaj-jj <sahajjain13@gmail.com>
2023-04-30 16:54:37 +0530

    feat: Add fixed_window limiter with major refactor

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -7,31 +7,10 @@ const rateLimiterMiddleware = async (req, res, next) => {
         const rateLimiter = new RateLimiter({ redisClient });
         const limiterParams = await redisClient.hgetall("limiterParams");
 
-        const scheme = limiterParams.scheme || DEFAULT_SCHEME;
-
-        let isAllowed = false;
-
-        if (scheme === RATE_LIMITER_SCHEMES.SLIDING_WINDOW) {
-            const { max_requests_per_window, window_size_in_ms } =
-                limiterParams;
-
-            isAllowed = await rateLimiter.slidingWindow({
-                key: req.ip,
-                maxRequestsPerWindow:
-                    parseInt(max_requests_per_window) || undefined,
-                windowSizeInMs: parseInt(window_size_in_ms) || undefined,
-            });
-        } else if (scheme === RATE_LIMITER_SCHEMES.TOKEN_BUCKET) {
-            const { bucket_size, refill_rate_in_ms } = limiterParams;
-
-            isAllowed = await rateLimiter.tokenBucket({
-                key: req.ip,
-                bucketSize: parseInt(bucket_size) || undefined,
-                refillRateInMs: parseInt(refill_rate_in_ms) || undefined,
-            });
-        } else {
-            throw new Error(`Unknown rate limiter scheme: ${scheme}`);
-        }
+        let isAllowed = await rateLimiter.isAllowed({
+            key: req.ip,
+            ...limiterParams,
+        });
 
         if (isAllowed) {
             return next();
--- src/rateLimiter.js
+++ src/rateLimiter.js
@@ -1,15 +1,92 @@
-import { SLIDING_WINDOW_DEFAULTS, TOKEN_BUCKET_DEFAULTS } from "./constants.js";
+import {
+    FIXED_WINDOW,
+    FIXED_WINDOW_DEFAULTS,
+    SLIDING_WINDOW,
+    SLIDING_WINDOW_DEFAULTS,
+    TOKEN_BUCKET,
+    TOKEN_BUCKET_DEFAULTS,
+} from "./constants.js";
+
+import applyDefaults from "./utils/applyDefaults.js";
 
 export const RateLimiter = class {
     constructor({ redisClient }) {
         this.redisClient = redisClient;
     }
 
-    async slidingWindow({
-        key,
-        windowSizeInMs = SLIDING_WINDOW_DEFAULTS.window_size_in_ms,
-        maxRequestsPerWindow = SLIDING_WINDOW_DEFAULTS.max_requests_per_window,
-    }) {
+    async isAllowed({ key, scheme, ...options }) {
+        const limiter = this._getLimiter(scheme, options);
+        return limiter.isAllowed(key);
+    }
+
+    _getLimiter(scheme = SLIDING_WINDOW, options) {
+        switch (scheme) {
+            case FIXED_WINDOW:
+                const fixedWindowOptions = applyDefaults(
+                    options,
+                    FIXED_WINDOW_DEFAULTS
+                );
+                return new FixedWindowLimiter({
+                    redisClient: this.redisClient,
+                    ...fixedWindowOptions,
+                });
+            case SLIDING_WINDOW:
+                const slidingWindowOptions = applyDefaults(
+                    options,
+                    SLIDING_WINDOW_DEFAULTS
+                );
+                return new SlidingWindowLimiter({
+                    redisClient: this.redisClient,
+                    ...slidingWindowOptions,
+                });
+            case TOKEN_BUCKET:
+                const tokenBucketOptions = applyDefaults(
+                    options,
+                    TOKEN_BUCKET_DEFAULTS
+                );
+                return new TokenBucketLimiter({
+                    redisClient: this.redisClient,
+                    ...tokenBucketOptions,
+                });
+            default:
+                throw new Error(`Invalid algorithm: ${this.algorithm}`);
+        }
+    }
+};
+
+class FixedWindowLimiter {
+    constructor({ redisClient, windowSizeInMs, maxRequestsPerWindow }) {
+        this.redisClient = redisClient;
+        this.windowSizeInMs = windowSizeInMs;
+        this.maxRequestsPerWindow = maxRequestsPerWindow;
+    }
+
+    async isAllowed(key) {
+        const now = Date.now();
+        const windowStart =
+            Math.floor(now / this.windowSizeInMs) * this.windowSizeInMs;
+        const windowEnd = windowStart + this.windowSizeInMs;
+        const count = await this.redisClient.zcount(
+            key,
+            windowStart,
+            windowEnd - 1
+        );
+        const isAllowed = count < this.maxRequestsPerWindow;
+        if (isAllowed) {
+            await this.redisClient.zadd(key, now, now);
+        }
+        return isAllowed;
+    }
+}
+
+class SlidingWindowLimiter {
+    constructor({ redisClient, windowSizeInMs, maxRequestsPerWindow }) {
+        this.redisClient = redisClient;
+        this.windowSizeInMs = windowSizeInMs;
+        this.maxRequestsPerWindow = maxRequestsPerWindow;
+    }
+
+    async isAllowed(key) {
         const now = Date.now();
         const score = now;
 
@@ -17,22 +94,25 @@ export const RateLimiter = class {
         await this.redisClient.zadd(key, score, score);
 
         // Remove any entries from the set that are older than the sliding window
-        const oldestAllowedScore = now - windowSizeInMs;
-
+        const oldestAllowedScore = now - this.windowSizeInMs;
         await this.redisClient.zremrangebyscore(key, 0, oldestAllowedScore);
 
         // Count the number of remaining entries in the set
         const count = await this.redisClient.zcard(key);
 
         // Return true if the number of entries is less than the maximum allowed
-        return count <= maxRequestsPerWindow;
+        return count <= this.maxRequestsPerWindow;
+    }
+}
+
+const TokenBucketLimiter = class {
+    constructor({ redisClient, bucketSize, refillRateInMs }) {
+        this.redisClient = redisClient;
+        this.bucketSize = bucketSize;
+        this.refillRateInMs = refillRateInMs;
     }
 
-    async tokenBucket({
-        key,
-        bucketSize = TOKEN_BUCKET_DEFAULTS.bucket_size,
-        refillRateInMs = TOKEN_BUCKET_DEFAULTS.refill_rate_in_ms,
-    }) {
+    async isAllowed(key) {
         const now = Date.now();
         const timestampKey = `${key}:lastRefillTimestamp`;
         const bucketKey = `${key}:bucket`;
@@ -44,16 +124,19 @@ export const RateLimiter = class {
         let tokensToAdd = 0;
         if (lastRefillTimestamp) {
             const elapsedTime = now - parseInt(lastRefillTimestamp);
-            tokensToAdd = Math.floor(elapsedTime / refillRateInMs);
+            tokensToAdd = Math.floor(elapsedTime / this.refillRateInMs);
         } else {
-            tokensToAdd = bucketSize;
+            tokensToAdd = this.bucketSize;
         }
 
         // Refill the bucket with new tokens
         const currentTokens = parseInt(
             (await this.redisClient.get(bucketKey)) || 0
         );
-        const newTokens = Math.min(currentTokens + tokensToAdd, bucketSize);
+        const newTokens = Math.min(
+            currentTokens + tokensToAdd,
+            this.bucketSize
+        );
         await this.redisClient.set(bucketKey, newTokens);
 
         // Check if there are enough tokens in the bucket
