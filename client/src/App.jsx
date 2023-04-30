9a72a8554f988c5ca418fc5a75d2997ab8bfcd57
Sahaj-jj <sahajjain13@gmail.com>
2023-04-30 19:48:53 +0530

    feat: Display successful requests per minute stat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,14 @@ function App() {
     const [totalRequests, setTotalRequests] = useState(0);
     const [limitedRequests, setLimitedRequests] = useState(0);
     const { start, reset, isActive, time } = useTimer();
+    const [firstRequestTimestamp, setFirstRequestTimestamp] = useState(0);
 
     // Timestamps: firstRequest, lastRequest, lastSuccessfulRequest, firstSuccessfulRequest
+
+    const [timeDifference, setTimeDifference] = useState(0);
+
+    const successfulRequestsPerMinute =
+        (totalRequests - limitedRequests) / (timeDifference / (60 * 1000));
 
     const handleLimit = async () => {
         const result = await limit();
@@ -22,6 +28,9 @@ function App() {
         }
         if (!isActive) {
             start();
+            setFirstRequestTimestamp(Date.now());
+        } else {
+            setTimeDifference(Date.now() - firstRequestTimestamp);
         }
     };
 
@@ -76,6 +85,24 @@ function App() {
                             </div>
                             <div className="stat-name">
                                 <span>Limited Requests</span>
+                            </div>
+                        </div>
+                        <div className="stat-cont">
+                            <div className="stat-value">
+                                <span>
+                                    {successfulRequestsPerMinute.toFixed(2)}
+                                </span>
+                            </div>
+                            <div className="stat-name">
+                                <span>Successful Requests per Minute</span>
+                            </div>
+                        </div>
+                        <div className="stat-cont">
+                            <div className="stat-value">
+                                <span>{time}</span>
+                            </div>
+                            <div className="stat-name">
+                                <span>Time Elapsed</span>
                             </div>
                         </div>
                     </Grid>
